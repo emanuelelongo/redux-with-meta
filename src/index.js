e7d9b88e04c29b881345438871e80901eca55c6f
Emanuele Longo <longo.emanuele@gmail.com>
2018-07-22 18:49:41 +0200

    new variant withMetaArgument

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 const isThunkAction = action => typeof action == 'function';
-const interceptor = meta => action => ({...action, meta});
-const thunkInterceptor = dispatch => meta => action => dispatch({...action, meta});
+const interceptor = (meta, action) => ({...action, meta});
+const thunkInterceptor = (dispatch, meta) => action => dispatch({...action, meta});
 
 const withMeta = meta => actionCreator => function() {
     const action = actionCreator(...arguments);
     if(isThunkAction(action)) {
-        return dispatch => action(thunkInterceptor(dispatch)(meta));
+        return dispatch => action(thunkInterceptor(dispatch, meta));
     }
     else {
-        return interceptor(meta)(action);
+        return interceptor(meta, action);
     }
 }
 
-export default withMeta;
-
+const withMetaArgument = actionCreator => function() {
+    const action = actionCreator(...arguments);
+    const meta = arguments.length > 0 
+        ? arguments[arguments.length-1]
+        : {};
+    
+    if(isThunkAction(action)) {
+        return dispatch => action(thunkInterceptor(dispatch, meta));
+    }
+    else {
+        return interceptor(meta, action);
+    }
+}
+export {withMeta, withMetaArgument};
